8b00bf162893e8aee524221af34e55ff7dc1c684
takuowake <takuowake@gmail.com>
2023-02-19 19:11:16 +0900

    トークループページの基盤作成と画面遷移の設定

---
+++ lib/pages/talk_room_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TalkRoomPage extends StatefulWidget {
+  final String name;
+  const TalkRoomPage(this.name, {Key? key}) : super(key: key);
+
+  @override
+  State<TalkRoomPage> createState() => _TalkRoomPageState();
+}
+
+class _TalkRoomPageState extends State<TalkRoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.name)
+      ),
+    );
+  }
+}
--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/model/user.dart';
+import 'package:chatapp/pages/talk_room_page.dart';
 import 'package:flutter/material.dart';
 
 class TopPage extends StatefulWidget {
@@ -24,28 +25,35 @@ class _TopPageState extends State<TopPage> {
       body: ListView.builder(
           itemCount: userList.length,
           itemBuilder: (context, index) {
-            return SizedBox(
-              height: 70,
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: userList[index].imagePath == null
-                        ? null
-                        : NetworkImage(userList[index].imagePath!),
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => TalkRoomPage(userList[index].name)
+                ));
+              },
+              child: SizedBox(
+                height: 70,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: userList[index].imagePath == null
+                          ? null
+                          : NetworkImage(userList[index].imagePath!),
+                      ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(userList[index].name, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
-                      Text(userList[index].lastMessage, style: const TextStyle(color: Colors.grey),)
-                    ],
-                  )
-                ],
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(userList[index].name, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                        Text(userList[index].lastMessage, style: const TextStyle(color: Colors.grey),)
+                      ],
+                    )
+                  ],
+                ),
               ),
             );
           }
