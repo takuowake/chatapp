55ce965916324c3de5be8abe32d0142fdc5bd027
takuowake <takuowake@gmail.com>
2023-02-19 18:59:58 +0900

    トップページのUIを作成

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -11,17 +11,45 @@ class TopPage extends StatefulWidget {
 class _TopPageState extends State<TopPage> {
   List<User> userList = [
     User(name: 'name1', uid: 'uid1',
-         imagePath: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.finnair.com%2Fen%2Fdestinations%2Famericas%2Fusa%2Flos-angeles&psig=AOvVaw27VlRpAzQP1usmyl6RU6sX&ust=1676861064934000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJi9ybTIoP0CFQAAAAAdAAAAABAE',
+         imagePath: 'https://cdn-images-1.medium.com/max/1200/1*ilC2Aqp5sZd1wi0CopD1Hw.png',
          lastMessage: 'thank you'),
     User(name: 'name2', uid: 'uid2',
-        imagePath: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.finnair.com%2Fen%2Fdestinations%2Famericas%2Fusa%2Flos-angeles&psig=AOvVaw27VlRpAzQP1usmyl6RU6sX&ust=1676861064934000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJi9ybTIoP0CFQAAAAAdAAAAABAE',
+        imagePath: 'https://cdn-images-1.medium.com/max/1200/1*ilC2Aqp5sZd1wi0CopD1Hw.png',
         lastMessage: 'your welcome'),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('チャットアプリ'),),
-      body: Center(child: const Text('メイン画面です')),
+      body: ListView.builder(
+          itemCount: userList.length,
+          itemBuilder: (context, index) {
+            return SizedBox(
+              height: 70,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: userList[index].imagePath == null
+                        ? null
+                        : NetworkImage(userList[index].imagePath!),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(userList[index].name, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                      Text(userList[index].lastMessage, style: const TextStyle(color: Colors.grey),)
+                    ],
+                  )
+                ],
+              ),
+            );
+          }
+      ),
     );
   }
 }
