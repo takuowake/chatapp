25bf031623ca1eeb2f2ee33f1bfb60d8f22d2dee
takuowake <takuowake@gmail.com>
2023-02-20 22:53:00 +0900

    トークルームのUI設定/メッセージ送信/情報設定画面

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:chatapp/firebase_options.dart';
 import 'package:chatapp/pages/top_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
